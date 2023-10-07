ea615a9ba30babc5053dbd6543804314257daa0f
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-07 21:21:24 +0200

    New update

---
+++ src/app/get-school/all/routeasdad.ts
@@ -0,0 +1,13 @@
+// import { failedToGetData, successNoData, successRequest } from "@/lib/api-return";
+// import { getSchools } from "@/lib/scrapeFunctions";
+
+// export async function GET() {
+//   const result = await getSchools();
+//   if (result === "No data") {
+//     return successNoData(result);
+//   } else if (result === null) {
+//     return failedToGetData();
+//   } else {
+//     return successRequest(result);
+//   }
+// }
