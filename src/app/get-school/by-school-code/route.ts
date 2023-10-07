22e797fdb847f9de4e0e9ae456f9514195325993
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-07 21:11:34 +0200

    New update

---
+++ src/app/get-school/by-school-code/route.ts
@@ -0,0 +1,27 @@
+import { failedToGetData, invalidParameters, successNoData, successRequest } from "@/lib/api-return";
+import { getSearchParamsObject } from "@/lib/getSearchParamsObject";
+import { getSchool } from "@/lib/scrapeFunctions";
+import { NextRequest } from "next/server";
+import z from "zod";
+
+const routeSchema = z.object({
+  schoolCode: z.string(),
+});
+
+export async function GET(request: NextRequest) {
+  const params = getSearchParamsObject(request);
+
+  try {
+    const data = routeSchema.parse(params);
+    const result = await getSchool(data);
+    if (result === "No data") {
+      return successNoData(result);
+    } else if (result === null) {
+      return failedToGetData();
+    } else {
+      return successRequest(result);
+    }
+  } catch {
+    return invalidParameters();
+  }
+}
