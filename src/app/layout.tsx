074b1b96d2229e5a858d517c03334c4606a4b47d
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-15 11:20:09 +0200

    New update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,10 +11,7 @@ export const metadata: Metadata = {
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div>Lectio API 2023</div>
-        <p>Get started here:</p>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
