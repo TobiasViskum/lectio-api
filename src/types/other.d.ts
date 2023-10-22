52c5e408fc6805772da47082221705b1d248151c
Tobias Tranberg Viskum <tobiastv@Tobiass-MacBook-Pro.local>
2023-10-22 19:55:21 +0200

    Minor update

--- src/types/other.d.ts
+++ src/types/other.d.ts
@@ -1,9 +1,31 @@
-type Pages = "front" | "schedule" | "absence-overview" | "absence-reasons" | "assignments" | "homework" | "grades-overview" | "grades-messages" | "borrowed-books" | "questionnaire" | "messages-newest" | "messages-unread" | "messages-personal" | "messages-all" | "messages-deleted" | "studyPlan-calender" | "studyPlan-educationDescription" | "studyPlan-studyDirection" | "studyPlan-elective" | "teachers" | "student-by-credentials";
+type Pages =
+  | "front"
+  | "schedule"
+  | "absence-overview"
+  | "absence-reasons"
+  | "assignments"
+  | "homework"
+  | "grades-overview"
+  | "grades-messages"
+  | "borrowed-books"
+  | "questionnaire"
+  | "messages-newest"
+  | "messages-unread"
+  | "messages-personal"
+  | "messages-all"
+  | "messages-deleted"
+  | "studyPlan-calender"
+  | "studyPlan-educationDescription"
+  | "studyPlan-studyDirection"
+  | "studyPlan-elective"
+  | "teachers"
+  | "student-by-credentials";
 
 type GetPageReturn =
   | "Not authenticated"
   | "Invalid school"
   | "No data"
+  | "Forbidden access"
   | {
       $: cheerio.Root;
       client: AxiosInstance;
